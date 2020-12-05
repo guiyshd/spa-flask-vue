18fc66bcb19b146a064c9e141f49f4f15f727fea
guiyshd <64761267+guiyshd@users.noreply.github.com>
2020-12-05 16:17:37 -0300

    Cors

--- flaskvue/run.py
+++ flaskvue/run.py
@@ -6,6 +6,7 @@ app = Flask(__name__,
             static_folder = "./dist/static",
             template_folder = "./dist")
 
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/random')
 def random_number():
@@ -13,10 +14,8 @@ def random_number():
         'randomNumber': randint(1, 100)
     }
     return jsonify(response)
-
     
 @app.route('/', defaults={'path': ''})
-
 
 @app.route('/<path:path>')
 def catch_all(path):
