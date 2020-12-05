a6af9204f7a745cc985579c7079b523d86251616
guiyshd <64761267+guiyshd@users.noreply.github.com>
2020-12-05 16:16:15 -0300

    Get Random From Backend

--- flaskvue/frontend/src/components/Home.vue
+++ flaskvue/frontend/src/components/Home.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
@@ -14,17 +15,20 @@ export default {
     }
   },
   methods: {
-    getRandomInt (min, max) {
-      min = Math.ceil(min)
-      max = Math.floor(max)
-      return Math.floor(Math.random() * (max - min + 1)) + min
-    },
     getRandom () {
-      this.randomNumber = this.getRandomInt(1, 100)
+      // this.randomNumber = this.getRandomInt(1, 100)
+      this.randomNumber = this.getRandomFromBackend()
+    },
+    getRandomFromBackend () {
+      const path = `http://localhost:5000/api/random`
+      axios.get(path)
+      .then(response => {
+        this.randomNumber = response.data.randomNumber
+      })
+      .catch(error => {
+        console.log(error)
+      })
     }
-  },
-  created () {
-    this.getRandom()
   }
 }
 </script>
